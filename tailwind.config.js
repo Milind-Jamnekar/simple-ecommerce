af20b506f432ca28d4380e43a9009ac90586f545
Milind Jamnekar <milindjamnekar502@gmail.com>
2024-10-12 08:15:15 +0530

    forgor to add this config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import { nextui } from "@nextui-org/react";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -8,5 +10,6 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
